757f0c081670f978bfb30cead334fd93f6ee9459
Swayam Prakash <swayam.prakash@dreamorbit.in>
2023-08-14 15:27:13 +0530

    [Swayam] : added functionality of add contact and display contact

--- src/com/dev/addressbook/AddressBookMain.java
+++ src/com/dev/addressbook/AddressBookMain.java
@@ -1,10 +1,38 @@
 package com.dev.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to Address Book Management System");
+		
+		
+		AddressBook addressbook = new AddressBook();
+		Scanner sc = new Scanner(System.in);
+		
+		int operationNo;
+		
+		do {
+			System.out.println("Welcome to Address Book Management System");
+			System.out.println("1. ADD CONTACT \n2. DISPLAY CONTACT \3. EXIT");
+			System.out.println("Enter the operation number : ");
+			operationNo = sc.nextInt();
+			
+			switch(operationNo) {
+			
+			case 1: 
+				addressbook.addContact();
+				break;
+			case 2:
+				addressbook.displayContact();
+				break;
+			default:
+				System.out.println("Wrong Operation Number ");
+				break;
+			}
+			
+		} while (operationNo != 3);
 	}
 
 }
