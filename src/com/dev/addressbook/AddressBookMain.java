49f52c187d4aff001aec8c6d062d825354b89921
Swayam Prakash <swayam.prakash@dreamorbit.in>
2023-08-14 14:48:26 +0530

    [Swayam] : created address book application and main class

---
+++ src/com/dev/addressbook/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.dev.addressbook;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Address Book Management System");
+	}
+
+}
